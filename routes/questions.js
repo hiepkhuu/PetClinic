8cfee01260c7032c1c8496b57e1900f3b8b5ef81
hiep <hiephkhuu@gmail.com>
2021-06-03 09:46:53 -0700

    added questions list, quesiton-form, and question-page. moved some pug around

--- routes/questions.js
+++ routes/questions.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
-const { ResultWithContext } = require('express-validator/src/chain');
 const {asyncHandler, csrfProtection} = require('./utils');
-const {requireAuth} = reQUIRE('../auth')
-const { Question, Answer, User} = requrie('../db/models');
+const {requireAuth} = require("../auth");
+const {Question, Answer, User} = require("../db/models");
+const { ResultWithContext } = require('express-validator/src/chain');
 
 
 //get Q with id and render Q + answers
@@ -14,3 +14,16 @@ router.get('/:id(\\d+)', asyncHandler(async(req, res)=>{
   })
    res.render('question-page', {question});
 }))
+
+const questionValidator = [
+  check('question')
+    .exists({checkFalsy: true})
+    .withMessage('Please enter a question')
+]
+
+//create page so users can enter their questions
+router.get('/add', csrfProtection, requireAuth, asyncHandler(async(req, res)=>{
+  res.render('question-form', {csrfToken: req.csrfToken()})
+}))
+
+module.exports = router
